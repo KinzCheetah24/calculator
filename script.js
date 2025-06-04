b5478c362dbbc0de56e6beaee4e5e4e8c64d29e4
Álvaro Pintado <alvaro.pintado@alumnos.upm.es>
2025-06-05 00:02:52 +0200

    Button operation logic added
    The logic for giving the result of the operations passed through pressing the buttons was made

--- script.js
+++ script.js
@@ -26,3 +26,53 @@ function operate(a, b, operator) {
             return divide(a, b);
     }
 }
+
+const displayContent = document.querySelector(".display .operation");
+const numberBtns = document.querySelectorAll(".numberBtn");
+const operatorBtns = document.querySelectorAll(".operatorBtn");
+const clearBtn = document.querySelector(".clearBtn");
+const equalBtn = document.querySelector(".equalBtn");
+let displayNumber = "", firstNumber, secondNumber, operation = "", operationTriggered = false;
+
+numberBtns.forEach((number) => {
+    number.addEventListener("click", () => {
+        if(!operationTriggered) {
+            displayContent.textContent += number.textContent;
+            displayNumber += number.textContent;
+        } else {
+            firstNumber = displayNumber;
+            displayContent.textContent = number.textContent;
+            displayNumber = number.textContent;
+            operationTriggered = false;
+        }
+    })
+});
+
+operatorBtns.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        if(operation === "") {
+            firstNumber = displayNumber;
+            displayContent.textContent += operator.textContent;
+            displayNumber = "";
+            operation = operator.textContent;
+        } else {
+            secondNumber = displayNumber;
+            displayNumber = operate(parseInt(firstNumber), parseInt(secondNumber), operation);
+            displayContent.textContent = displayNumber;
+            operation = operator.textContent;
+            operationTriggered = true
+        }
+    })
+});
+
+clearBtn.addEventListener("click", () => {
+    displayContent.textContent = "";
+    displayNumber = "", firstNumber = "", secondNumber = "", operation = "";
+});
+
+equalBtn.addEventListener("click", () => {
+    secondNumber = displayNumber;
+    displayNumber = operate(parseInt(firstNumber), parseInt(secondNumber), operation);
+    displayContent.textContent = displayNumber;
+    firstNumber = "", secondNumber = "", operation = "";
+});
